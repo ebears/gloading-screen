234eab5833e2165bff22cb2423e495674c71135a
ebears <thomlbmclean@gmail.com>
2022-02-23 02:40:54 -0500

    oops

--- js/config.js
+++ js/config.js
@@ -25,7 +25,7 @@ var l_bgVideo = true;
 // Just make sure they are using webm VP8 format 
 // (Place the videos in the backgrounds/videos folder)
 // (You can also use external URL)
-var l_background = "myabstract.webm";
+var l_background = "abstract.webm";
 
 /*(Image-background only)
 Place one or more images
